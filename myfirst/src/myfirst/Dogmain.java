7fa7682d139d896f12c2ffcc80a1c9291a68529b
LeeDoho <justwithus@naver.com>
2014-07-15 17:14:00 +0900

    hw1

---
+++ myfirst/src/myfirst/Dogmain.java
@@ -0,0 +1,30 @@
+package myfirst;
+
+public class Dogmain {
+
+	public static void main(String[] args) {
+		int i = 100;
+		
+		//Dog dog = new Dog("뽀삐", "");
+		//Dog dog = new Dog();와와
+		//dog.name = "똥개";
+		//dog.type = "푸들";
+		
+		
+		//System.out.println(dog.name);
+		//System.out.println(dog.type);
+
+		//System.out.println(dog);
+		
+		Man man = new Man("James", 40);
+		man.isMarried = true;
+		man.howManyChild = 3;
+		
+		System.out.println(man.age);
+		System.out.println(man.name);
+		System.out.println(man.isMarried);
+		System.out.println(man.howManyChild);
+		
+	}
+
+}
---
+++ myfirst/src/myfirst/Man.java
@@ -0,0 +1,13 @@
+package myfirst;
+
+public class Man {
+	public int age;
+	public String name;
+	public boolean isMarried;
+	public int howManyChild;
+	
+	public Man (String name, int age){
+		this.age = age;
+		this.name = name;
+	}
+}
